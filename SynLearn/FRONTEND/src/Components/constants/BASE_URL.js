2c6fb9fdc464813c376daa25adfaf23478db7dea
Raushan Raj <kumarraushanraj10@gmail.com>
2025-10-15 15:35:51 +0530

    all set 2

--- SynLearn/FRONTEND/src/Components/constants/BASE_URL.js
+++ SynLearn/FRONTEND/src/Components/constants/BASE_URL.js
@@ -1,2 +1,11 @@
-const BASE_URL = "http://localhost:5000";
+const BASE_URL =
+location.hostname === "localhost"
+    ? "http://localhost:5000"  // local backend
+    : "https://ajeet-sir-synlearn.onrender.com"; // production backend
+
 export default BASE_URL;
+
+
+
+
+// https://ajeet-sir-synlearn.onrender.com/-
